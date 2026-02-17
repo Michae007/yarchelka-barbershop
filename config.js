f60d545600944bffbb34c955ea91e71d0fae7510
Michae007 <124517503+Michae007@users.noreply.github.com>
2026-02-17 20:36:31 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 const SUPABASE_URL = 'https://vhsptoiylsalrunjjfsf.supabase.co'; // Ваш Project URL
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoc3B0b2l5bHNhbHJ1bmpqZnNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzMzIwMDcsImV4cCI6MjA4NjkwODAwN30.BvXTD1O-eNwCmQaVbTzOVvDUMTuny-YJW7460R1WNg8'; // Ваш anon / public ключ
 
-// Инициализация Supabase
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// Инициализация Supabase (используем другое имя, чтобы избежать конфликтов)
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // Простая проверка авторизации для панели мастера
 const MASTER_PASSWORD = "Lavesi574482"; // Поменяйте на свой надежный пароль
