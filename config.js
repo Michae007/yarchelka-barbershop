c792dcfeee2128d226825057e9c0f1b0c7fa3718
Michae007 <124517503+Michae007@users.noreply.github.com>
2026-02-17 22:32:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // ЗАМЕНИТЕ ЭТИ ЗНАЧЕНИЯ НА СВОИ С SUPABASE!
 const SUPABASE_URL = 'https://vhsptoiylsalrunjjfsf.supabase.co'; // Ваш Project URL
-const SUPABASE_ANON_KEY = 'sb_publishable_tfwnKOIycYeeWn-ExIsc4A_WxLWWvQ3'; // Ваш anon / public ключ
+const SUPABASE_ANON_KEY = 'sb_secret_qDSVNi1JOaooSTDNlvUORQ_E8Eg8Dj0 '; // Ваш anon / public ключ
 
 // Инициализация Supabase (используем другое имя, чтобы избежать конфликтов)
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
