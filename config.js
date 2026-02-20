52236c92729c3d71dab5e2bde00c05b3305c220a
Michae007 <124517503+Michae007@users.noreply.github.com>
2026-02-20 12:14:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // ЗАМЕНИТЕ ЭТИ ЗНАЧЕНИЯ НА СВОИ С SUPABASE!
 const SUPABASE_URL = 'https://vhsptoiylsalrunjjfsf.supabase.co'; // Ваш Project URL
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoc3B0b2l5bHNhbHJ1bmpqZnNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzMzIwMDcsImV4cCI6MjA4NjkwODAwN30.BvXTD1O-eNwCmQaVbTzOVvDUMTuny-YJW7460R1WNg8 '; // Ваш anon / public ключ
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoc3B0b2l5bHNhbHJ1bmpqZnNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzMzIwMDcsImV4cCI6MjA4NjkwODAwN30.BvXTD1O-eNwCmQaVbTzOVvDUMTuny-YJW7460R1WNg8'; // Ваш anon / public ключ
 
 // Инициализация Supabase (используем другое имя, чтобы избежать конфликтов)
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
